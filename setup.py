0c987985610e1705e7ba2bee5d6627e3a19f76f9
edithvee <32566441+edithvee@users.noreply.github.com>
2017-10-08 11:56:04 +0200

    package template
    nothing to see here!

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="datavisapp",
+    version="0.1.0",
+    url="https://github.com/edithvee/datavisapp",
+
+    author="edithvee",
+    author_email="32566441+edithvee@users.noreply.github.com",
+
+    description="a data visualisation app _for science_",
+    long_description=open('README.md').read(),
+
+    packages=setuptools.find_packages(),
+
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
