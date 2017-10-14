f5438731239177bbbf1300772f32b708ff1c50e7
davistodt <davis.todt@roche.com>
2017-10-14 12:26:20 +0200

    Added generic function to append a csv to a db table

--- datavisapp/create_db.py
+++ datavisapp/create_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 
 def make_table(db, table_name, col_types):
@@ -13,3 +14,9 @@ def make_table(db, table_name, col_types):
     cursor.execute(query)
     conn.close()
 
+
+def append_csv_to_table(db, table_name, csv_path):
+    conn = sqlite3.connect(db)
+    df = pd.read_csv(csv_path)
+    df.to_sql(table_name, conn, if_exists='append', index=False)
+    conn.close()
--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from datavisapp.create_db import make_table
+from datavisapp.create_db import make_table, append_csv_to_table
 
 
 def test_make_table(tmpdir):
@@ -18,6 +18,27 @@ def test_make_table(tmpdir):
     existing_tables = cursor.fetchall()
     conn.close()
     assert ('metadata',) in existing_tables
+
+
+def test_append_csv_to_table(tmpdir):
+    db = str(tmpdir.join('test.db'))
+
+    col_types = [
+        ('Sample', 'str'),
+        ('MetricA', 'float'),
+        ('MetricB', 'float'),
+    ]
+    make_table(db, 'experiment1_metrics', col_types)
+    append_csv_to_table(db, 'experiment1_metrics', 'tests/data/DatasetX.csv')
+
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+    cursor.execute("select * from experiment1_metrics")
+    table_result = cursor.fetchall()
+    conn.close()
+    assert ('S01', 0.5, 20.0) and ('S02', 0.9, 45.0) in table_result
+
+
 
 # def test_insert():
 #     conn = sqlite3.connect("test_db")
