b8539507b08fe98dd42796a74bfec32377f46115
Jack Viljoen <javiljoen@users.noreply.github.com>
2017-10-16 08:37:21 +0200

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import setuptools
 
 setuptools.setup(
-    name="datavisapp",
-    version="0.1.0",
-    url="https://github.com/edithvee/datavisapp",
+    name='datavisapp',
+    version='0.1.0',
+    url='https://github.com/edithvee/datavisapp',
 
-    author="edithvee",
-    author_email="32566441+edithvee@users.noreply.github.com",
+    author='edithvee',
+    author_email='32566441+edithvee@users.noreply.github.com',
 
-    description="a data visualisation app _for science_",
+    description='a data visualisation app _for science_',
     long_description=open('README.rst').read(),
 
-    packages=setuptools.find_packages(),
+    packages=['datavisapp'],
 
     install_requires=[
         'pandas',
