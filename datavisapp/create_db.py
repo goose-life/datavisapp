ddad893199b80538c921a3e15598496389762bce
davistodt <davis.todt@roche.com>
2017-10-17 11:13:31 +0200

    Created function to return a list of tables from a database. Cleaned up code to wrap DB operations in 'with' statements Added a test for the new function.

--- datavisapp/create_db.py
+++ datavisapp/create_db.py
@@ -1,22 +1,46 @@
 import sqlite3
 import pandas as pd
 
+from itertools import chain
 
-def make_table(db, table_name, col_types):
-    conn = sqlite3.connect(db)
-    cursor = conn.cursor()
+
+def make_table(table_name, col_types, db_name=':memory:'):
+    """
+    Create a new table in a database using column names and types supplied.
+    """
     columns = []
     for col_name, col_type in col_types:
         col_str = f'{col_name} {col_type}'
         columns.append(col_str)
     columns = ', '.join(columns)
     query = f'''create table {table_name} ({columns})'''
-    cursor.execute(query)
-    conn.close()
+
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    with conn:
+        c.execute(query)
 
 
-def append_csv_to_table(db, table_name, csv_path):
-    conn = sqlite3.connect(db)
-    df = pd.read_csv(csv_path)
-    df.to_sql(table_name, conn, if_exists='append', index=False)
-    conn.close()
+def append_csv_to_table(table_name, csv_path, db_name=':memory:'):
+    """
+    Append the data from a csv file to a table in the database.
+    """
+    conn = sqlite3.connect(db_name)
+
+    with conn:
+        df = pd.read_csv(csv_path)
+        df.to_sql(table_name, conn, if_exists='append', index=False)
+
+
+def get_tables_list(db_name=':memory:'):
+    """
+    Return a list of all the tables in a database.
+    """
+    conn = sqlite3.connect(db_name)
+    c = conn.cursor()
+
+    with conn:
+        c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        result = list(chain.from_iterable(c.fetchall()))
+        return result
--- tests/test_create_db.py
+++ tests/test_create_db.py
@@ -1,22 +1,24 @@
 import sqlite3
 
-from datavisapp.create_db import make_table, append_csv_to_table
+from datavisapp.create_db import make_table, append_csv_to_table, get_tables_list
 
 
 def test_make_table(tmpdir):
     db = str(tmpdir.join('test.db'))
 
     col_types = [
-        ('DataSet', 'str'),
-        ('Date', 'str'),
+        ('DataSet', 'TEXT'),
+        ('Date', 'TEXT'),
     ]
-    make_table(db, 'metadata', col_types)
+    make_table('metadata', col_types, db_name=db)
 
     conn = sqlite3.connect(db)
-    cursor = conn.cursor()
-    cursor.execute("select name from sqlite_master where type = 'table'")
-    existing_tables = cursor.fetchall()
-    conn.close()
+    c = conn.cursor()
+
+    with conn:
+        c.execute("select name from sqlite_master where type = 'table'")
+        existing_tables = c.fetchall()
+
     assert ('metadata',) in existing_tables
 
 
@@ -24,20 +26,34 @@ def test_append_csv_to_table(tmpdir):
     db = str(tmpdir.join('test.db'))
 
     col_types = [
-        ('Sample', 'str'),
-        ('MetricA', 'float'),
-        ('MetricB', 'float'),
+        ('Sample', 'TEXT'),
+        ('MetricA', 'REAL'),
+        ('MetricB', 'REAL'),
     ]
-    make_table(db, 'experiment1_metrics', col_types)
-    append_csv_to_table(db, 'experiment1_metrics', 'tests/data/DatasetX.csv')
+    make_table('experiment1_metrics', col_types, db_name=db)
+    append_csv_to_table('experiment1_metrics', 'tests/data/DatasetX.csv', db_name=db)
 
     conn = sqlite3.connect(db)
-    cursor = conn.cursor()
-    cursor.execute("select * from experiment1_metrics")
-    table_result = cursor.fetchall()
-    conn.close()
+    c = conn.cursor()
+
+    with conn:
+        c.execute("select * from experiment1_metrics")
+        table_result = c.fetchall()
+
     assert ('S01', 0.5, 20.0) and ('S02', 0.9, 45.0) in table_result
 
+
+def test_get_tables_list(tmpdir):
+    db = str(tmpdir.join('test.db'))
+
+    col_types = [
+        ('DataSet', 'str'),
+        ('Date', 'str'),
+    ]
+    make_table('metadata', col_types, db_name=db)
+    tables_list = get_tables_list(db)
+
+    assert 'metadata' in tables_list
 
 
 # def test_insert():
