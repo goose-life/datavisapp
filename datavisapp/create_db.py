8f36f0dc1956f4a5d9608017c3e0c4e11a6a0095
davistodt <davis.todt@roche.com>
2017-10-26 12:54:08 +0200

    add error handling and rollback of changes made to the database.
    create_db fn is wrapped in a try-except block to allow the program
    to continue if the database already exists. This is currently
    necessary since the creation of the db and inserting data have
    not yet been decoupled.
    
    add_data_to_db is wrapped in a try-except block to allow for
    changes to the database to be rolled back should an error occur
    at any stage in the addition of data to tables. For this to be
    possible, the db.transaction method has been introduced to
    keep track of changes made to the db. This can be used by
    calling either t.commit() or t.rollback() where applicable.
    
    The database connection object has also been moved to the top
    level to allow for greater control over database transactions.
    
    Note that tests have not yet been written for these changes.

--- datavisapp/create_db.py
+++ datavisapp/create_db.py
@@ -1,32 +1,29 @@
 import json
-import sqlite3
-import sqlalchemy
 from collections import OrderedDict
 
 import click
 import pandas as pd
 import records
+import sqlalchemy
 from pkg_resources import resource_filename
-
 
 default_schema = resource_filename(__name__, 'db_schema.json')
 
 
-def make_table(db_path, table_name, col_types):
+def make_table(db, table_name, col_types):
     """Create a new table in a SQLite database.
 
     If the database does not already exist then one will be created.
 
     Args:
-        db_path (str): path to the SQLite database.
+        db (:obj: `records.DataBase`): records.Database SQLite object.
         table_name (str): name of the table to be created.
         col_types (List[Tuple[str, str]]): column names and types to be used in the table.
 
-    Returns: None
+    Returns: None.
     """
     columns = ', '.join([f'{col_name} {col_type}' for col_name, col_type in col_types])
     query = f'CREATE TABLE {table_name} ({columns})'
-    db = records.Database(f'sqlite:///{db_path}')
     db.query(query)
 
 
@@ -34,11 +31,11 @@ def insert_into_db(db, table, data: list):
     """Append data to database table with matching field names.
 
     Args:
-        db (str): path to the SQLite database
-        table (str): name of the table in which to insert the data
-        data (List[Mapping]): a list of records
+        db (:obj: `records.DataBase`): records.Database SQLite object.
+        table (str): name of the table in which to insert the data.
+        data (List[Mapping]): a list of records.
 
-    Returns (List): list of row ids inserted into the table (useful for setting foreign keys)
+    Returns (List): list of row ids inserted into the table (useful for setting foreign keys).
     """
     fields = data[0].keys()
     field_list = ', '.join(fields)
@@ -46,12 +43,11 @@ def insert_into_db(db, table, data: list):
     query = f'INSERT INTO {table} ({field_list}) VALUES({placeholders})'
     last_row_ids = []
 
-    with records.Database(f'sqlite:///{db}') as db:
-        for record in data:
-            db.query(query, **record)
-            last_row_id = db.query(f'SELECT last_insert_rowid() AS id FROM {table}')  # returns last id inserted into {table}
-            last_row_id = last_row_id[0]["id"]  # RecordCollection -> Record -> Integer
-            last_row_ids.append(last_row_id)
+    for record in data:
+        db.query(query, **record)
+        last_row_id = db.query(f'SELECT last_insert_rowid() AS id FROM {table}')  # returns last id inserted into {table}
+        last_row_id = last_row_id[0]["id"]  # RecordCollection -> Record -> Integer
+        last_row_ids.append(last_row_id)
 
     return last_row_ids
 
@@ -60,11 +56,11 @@ def add_data_to_db(db, json_path, csv_path):
     """Add metadata and results from an analysis to the database.
 
     Args:
-        db (str): path to the SQLite database
-        json_path (Union[str, Path]): path to the metadata JSON file
-        csv_path (Union[str, Path]): path to the results CSV file
+        db (:obj: `records.DataBase`): records.Database SQLite object.
+        json_path (Union[str, Path]): path to the metadata JSON file.
+        csv_path (Union[str, Path]): path to the results CSV file.
 
-    Returns: None
+    Returns: None.
     """
     with open(json_path) as of:
         json_data = json.load(of)
@@ -87,7 +83,14 @@ def add_data_to_db(db, json_path, csv_path):
 
 
 def create_db(db, schema_json):
-    """Create a database according to schema in JSON format."""
+    """Create a database according to a schema in JSON format.
+
+    Args:
+        db (:obj: `records.DataBase`): records.Database SQLite object.
+        schema_json (Union[str, Path]): path to the JSON schema file.
+
+    Returns: None.
+    """
     with open(schema_json) as of:
         schema = json.load(of, object_pairs_hook=OrderedDict)
         # OrderedDict so that tables are created in the order specified,
@@ -117,5 +120,27 @@ def main(metadata_json, results_csv, db_path, schema_json):
     The schema can be supplied as a JSON file with the following structure:
     {"<table name>": {"<field name>": "<field type> <constraints>"}}
     """
-    create_db(db_path, schema_json)
-    add_data_to_db(db_path, metadata_json, results_csv)
+    db = records.Database(f'sqlite:///{db_path}')
+    t = db.transaction()
+
+    try:
+        create_db(db, schema_json)
+    except Exception as e:
+        click.echo(f'INFO: {e}'
+                   f'\nSkipping database setup.')
+        t.rollback()
+
+    try:
+        add_data_to_db(db, metadata_json, results_csv)
+    except sqlalchemy.exc.IntegrityError as e:
+        click.echo(f'ERROR: {e}'
+                   f'\nRolling back changes.')
+        t.rollback()
+        raise SystemExit
+    except Exception as e:
+        click.echo(f'ERROR: {e}'
+                   f'\nRolling back changes.')
+        t.rollback()
+        raise
+
+    t.commit()
